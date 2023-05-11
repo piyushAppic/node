31deee7c93ec2500dca56a810e51134b5494ad46
piyush <balajishri682@gmail.com>
2023-05-11 11:03:07 +0530

    how to exclude some fileds in populate method

--- controller/populate/userFetchByRef.js
+++ controller/populate/userFetchByRef.js
@@ -5,7 +5,10 @@ const Comment = require("../../model/comment")
 const findUserDeatilsByPost = async(req, res) => {
     try{
         const {post_id} = req.body
-        const user = await Post.findOne({_id: post_id}).populate("author_ref")
+        // const user = await Post.findOne({_id: post_id}).populate({path:"author_ref", select: ["name"]})
+        // exclude _id, email, and posts from author_ref json
+        const user = await Post.findOne({_id: post_id}).populate({path:"author_ref", select: "-_id -email -posts"})
+
         return res.json({user: user})
     }catch(err){
         return res.json({message: err.message})
