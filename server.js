dc9f87ec27225bca8f7f97cfaac776be8cfffd96
piyushAppic <piyush.shrimal@appicsoftwares.in>
2023-05-18 10:44:48 +0530

    feat - stripe payment

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const app = express()
+const path = require('path')
+
 require("./db")
 const PORT = 3000
+
+// const CorsPermission = require("./middlewares/corsPermission")
+// app.use(CorsPermission);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// View Engine Setup
+app.set('views', path.join(__dirname, 'payment/views'))
+app.set('view engine', 'ejs')
+
 const userRouter = require("./routers/userRouter")
 app.use("/api/v1/user", userRouter)
 
 app.use("*", (req, res)=> {
-    res.send("Hello")
+    res.send("Hello 404 page")
 })
 
 app.listen(PORT, () => {
